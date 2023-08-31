b8a981053dcfb3a36afb13b1a5aa3a1beb03ea38
Rawle Becerril <rawlesxm@gmail.com>
2023-08-30 20:37:49 -0400

    working

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,11 +2,39 @@ import React from 'react'
 
 const Homepage = () => {
 
-  const data = ["Rawle", "Becerril", ]
-  return (
+  const rawData = [
+    {
+    name: "Rawle",
+    age: 28,
+    state: "Maryland"
+  },
+  {
+    name: "John",
+    age: 40,
+    state: "California"
+  },
+  {
+    name: "Jill",
+    age: 50,
+    state: "New York"
+  }]
+
+  const data = rawData.map(d => ({
+    name: d.name,
+    age: d.age
+  }))
+
+  return (<>
+    <script src="https://d3js.org/d3.v7.min.js"></script>
     <div>
       <h1> Hey there</h1>
     </div>
+
+
+    <div id="chart-container">
+
+    </div>
+    </>
   )
 }
 
